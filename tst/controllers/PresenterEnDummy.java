15a8ba34785b4c4f1f3afa2600bedab396fd0a96
bravogolfgolf <bggibson@hotmail.com>
2016-04-14 02:05:22 +0200

    Refactor

--- tst/controllers/PresenterEnDummy.java
+++ tst/controllers/PresenterEnDummy.java
@@ -1,6 +1,41 @@
 package game.controllers;
 
-import game.presenters.PresenterEn;
+import game.interactors.createplayer.CreatePlayerResponse;
+import game.interactors.setupgame.SetupGameResponse;
 
-public class PresenterEnDummy extends PresenterEn {
+public class PresenterEnDummy extends Presenter {
+    @Override
+    public void playerCreatedMessage(CreatePlayerResponse response) {
+
+    }
+
+    @Override
+    public void tokenInUseMessage(CreatePlayerResponse response) {
+
+    }
+
+    @Override
+    public void exceededPlayerLimitMessage() {
+
+    }
+
+    @Override
+    public void playerPromptMessage() {
+
+    }
+
+    @Override
+    public void boardPromptMessage() {
+
+    }
+
+    @Override
+    public void boardCreatedMessage(SetupGameResponse response) {
+
+    }
+
+    @Override
+    public void availableBoardsMessage(SetupGameResponse response) {
+
+    }
 }
