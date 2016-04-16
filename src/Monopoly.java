8298b3cf9d6a7cdbc01ec7b5b72e7585bdf69d6d
bravogolfgolf <bggibson@hotmail.com>
2016-04-16 14:50:49 +0200

    Remove static reference to ControllerFactory

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -6,15 +6,13 @@ import java.io.IOException;
 
 public final class Monopoly {
 
-    public static ControllerFactory factory;
-
     public static void main(String[] args) throws IOException {
         Monopoly game = new Monopoly();
         game.start();
     }
 
     private void start() throws IOException {
-        factory = new ControllerFactoryImpl();
+        ControllerFactory factory = new ControllerFactoryImpl();
 
         Controller controller = factory.make("SetupGameController");
         controller.execute();
