9d628eb2e466caedcfe7f51b1c0360257301dc9e
bravogolfgolf <bggibson@hotmail.com>
2016-04-24 23:24:58 +0200

    Refactor

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -9,7 +9,7 @@ abstract class Controller implements ManagerController {
 
     ControllerInteractor interactor;
     ControllerPresenter presenter;
-    InteractorRequest request = new InteractorRequest();
+    final InteractorRequest request = new InteractorRequest();
 
     @Override
     public abstract void handle(String text) throws IOException;
